ba5f2736495cb0f3f9ad25716f21b2b642f20181
taichi221228 <taichi221228@icloud.com>
2022-07-10 20:49:19 +0900

    Add top page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,11 @@
+import 'sanitize.css';
+import 'sanitize.css/forms.css';
+import 'sanitize.css/assets.css';
+import 'sanitize.css/system-ui.css';
+import 'sanitize.css/typography.css';
+import 'sanitize.css/reduce-motion.css';
+import 'sanitize.css/ui-monospace.css';
+
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
